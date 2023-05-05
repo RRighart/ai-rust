7a571d330a6c659fdace0e865c59aea0ea6b5bf7
rrighart <rrighart@googlemail.com>
2023-05-05 14:46:15 +0200

    some tests in rust

---
+++ loop-test.rs
@@ -0,0 +1,17 @@
+fn main() {
+    //let a = [10, 20, 30, 40, 50];
+    //let mut index = 0;
+    //while index < 5 {
+    //    println!("the value is: {}", a[index]);
+    //    index = index + 1;
+
+    //let a = [10, 20, 30, 40, 50];
+    //for element in a {
+    //    println!("The value is: {}", element);
+    
+    for number in (1..4).rev() {
+
+        println!("{}!", number);
+
+    }
+}
